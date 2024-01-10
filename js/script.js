cbd828812f44e6c1d6b91170c2fdf2dff05149f7
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-01-10 18:07:31 -0300

    criando uma tela inicial

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+const telaInicial = document.querySelector(".telaInicial")
+const jogo = document.querySelector(".jogo")
+
+const btn_começar = document.querySelector(".btn_começar")
+const btn_proxima = document.querySelector(".btn_proxima")
+
+btn_começar.addEventListener("click",()=>{
+    telaInicial.style.display = "none"
+    jogo.style.display = "block"
+
+
+})
+
 const questoes = {
     id1: {
         Q1: "Quanto é 1 + 1?",
@@ -20,3 +33,14 @@ const questoes = {
     },
 }
 
+// let contador = "id"+ 1
+
+// btn_proxima.addEventListener("click",()=>{
+//     console.log(questoes.id1)
+//     console.log(contador)
+//     contador++
+// })
+
+for(el in questoes){
+    console.log(questoes[el])
+}
