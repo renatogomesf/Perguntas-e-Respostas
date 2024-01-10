3f24c2800e5337244a32b7545c2b74374ae28015
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-01-10 14:47:29 -0300

    inicializado o projeto

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const questoes = {
+    id1: {
+        Q1: "Quanto é 1 + 1?",
+        respostas: {
+            a: "2",
+            b: "1",
+            c: "4",
+            d: "3"
+        }
+    },
+
+    id2: {
+        Q2: "Quanto é 1 + 1?",
+        respostas: {
+            a: "2",
+            b: "22222",
+            c: "4",
+            d: "3"
+        }
+    },
+}
+
